a1be2373009417a087735087f0032b91e8a93ee0
Sushant <sushantkhadka2060@gmail.com>
2025-01-28 14:57:45 +0545

    database connection

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Database connection established");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server Listening on port 3000");
+});
