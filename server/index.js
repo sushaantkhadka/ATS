9b8cd93a7a755f12a844fcae50e60b43c7ba315a
Sushant <sushantkhadka2060@gmail.com>
2025-01-29 19:44:51 +0545

    hashing password

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import userRoutes from './routes/user.route.js'
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 
 import connectToDB from "./db/connectToDB.js";
@@ -9,10 +10,22 @@ import connectToDB from "./db/connectToDB.js";
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   connectToDB();
   console.log("Server Listening on port 3000");
 });
 
+app.use("/server/user", userRoutes);
+app.use("/server/auth", authRoutes);
 
-app.use("/server/user", userRoutes)
+app.use((err, req, res, next) => {
+  const statusCode= err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode
+  })
+})
